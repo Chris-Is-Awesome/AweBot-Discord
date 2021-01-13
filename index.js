5183f840457e06a8604bb7f667ef32e8417e6fa9
Chris is Awesome <chrisstarkofficial@gmail.com>
2021-01-12 23:47:53 -0800

    Get token

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
 
 client.on("ready", () => {
 	console.log("The client is ready!");
 })
 
-client.login(config.token);
+client.login(process.env.DJS_TOKEN);
