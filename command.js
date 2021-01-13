5463e6538a94bbf1c34c9082c5563b5589d80ad7
Chris is Awesome <chrisstarkofficial@gmail.com>
2021-01-13 01:18:16 -0800

    Some basic commands

---
+++ command.js
@@ -0,0 +1,21 @@
+const { prefix } = require("./config.json");
+
+module.exports = (client, aliases, callback) => {
+	if (typeof aliases === "string") {
+		aliases = [aliases];
+	}
+
+	client.on("message", message => {
+		const { content } = message;
+
+		aliases.forEach(aliases => {
+			const command = `${prefix}${aliases}`;
+
+			if (content.startsWith(`${command} `) || content === command) {
+				console.log("Running the command " + `${command}`);
+				callback(message);
+			}
+		})
+	})
+
+}
--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const command = require("./command");
+
 client.on("ready", () => {
 	console.log("The client is ready!");
+	client.user.setActivity("my humble servants", { type: 'WATCHING' });
+
+	command(client, "ping", (message) => {
+		message.channel.send("Pong!");
+	})
+
+	command(client, "servers", (message) => {
+		client.guilds.cache.forEach((guild) => {
+			message.channel.send(`${guild.name} has a total of ${guild.memberCount} members`);
+		})
+	})
+
+	command(client, ['cc', 'clearchannel'], message => {
+		if (message.member.hasPermission('ADMINISTRATOR')) {
+			message.channel.messages.fetch().then(results => {
+				message.channel.bulkDelete(results);
+			})
+		}
+	})
 })
 
 client.login(process.env.DJS_TOKEN);
